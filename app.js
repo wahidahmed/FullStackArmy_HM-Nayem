07408415d460a70ca219248629965058a4a3b3a2
shariareshuvo943 <shariareshuuvo@gmail.com>
2023-01-22 19:48:07 +0600

    another

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 
 
-const arr=[1,3,2,null,5,4,'','test',7];
+const arr=[1,3,2,null,5,false,4,'','test',7,'law'];
 
 for (let i = 0; i < arr.length; i++) {
-    for (let j  = i; j < array.length-1; j++) {
-       if (!arr[j] || typeof arr[j]!= 'number' ) {
-        
+    for (let j  = i; j < arr.length-1; j++) {
+       if ( typeof arr[j]!== 'number' ) {
+            arr[j]=arr[j+1];
        }
     }
 }
+
+console.log(arr);
