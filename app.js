ff3ddcc5e51b4c0a7a6650155e0e65ea1494f998
wahidahmed <wahidahmed1992@gmail.com>
2023-03-03 17:00:10 +0800

    start lecture 6

--- app.js
+++ app.js
@@ -1,23 +1,16 @@
 
-let arr=[
-{id:1,value:10},
-{id:2,value:20},
-{id:3,value:30},
-]
+const obj={
+    brand:'nokia',
+    price:5400,
+    country:'bag',
+    tools:()=>{
+        console.log('nokia is comming')
+    }
+}
 
-
-var index=arr.findIndex((v)=>{
-    return v.id==2;
-})
-
-console.log(index);
-arr[index]=200;
-
-console.log(arr);
-
-var obj=arr.find((v)=>{
-return v.id==index;
-})
-
-obj.value=100;
-console.log('obj',obj);
+for (const key in obj) {
+    if (Object.hasOwnProperty.call(obj, key)) {
+        const element = obj[key];
+        console.log('')
+    }
+}
