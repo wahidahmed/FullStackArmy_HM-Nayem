be9d13f2579b41fca0ac1e335e175362dce96939
wahidahmed <wahidahmed1992@gmail.com>
2023-03-17 12:28:30 +0800

    real ex for async await

--- app10.js
+++ app10.js
@@ -13,10 +13,31 @@
 // })
 
 
-const wait=(ms)=>new Promise((resolve)=>setTimeout(resolve,ms));
+// const wait=(ms)=>new Promise((resolve)=>setTimeout(resolve,ms));
 
-wait(1000).then(()=>console.log('done 1'));
+// wait(1000).then(()=>console.log('done 1'));
 
-wait(2000).then(()=>console.log('done 2'));
+// wait(2000).then(()=>console.log('done 2'));
 
-wait(3000).then(()=>console.log('done 3'));
+// wait(3000).then(()=>console.log('done 3'));
+
+
+const axios= require('axios').default;
+
+const userUrl=`https://jsonplaceholder.typicode.com/users`;
+const prostUrl=`https://jsonplaceholder.typicode.com/posts`;
+const commentUrl=`https://jsonplaceholder.typicode.com/comments`;
+
+const username='Bret';
+
+
+async function getComments(){
+    const {data:user}=await axios.get(`${userUrl}?username=${username}`);
+    console.log(user[0].id);
+    const {data:comment}=await axios.get(`${prostUrl}?userId=${user[0].id}`);
+    console.log(comment);
+}
+
+getComments();
+
+
