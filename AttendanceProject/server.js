9454f89476e609ffabf3dfbf7f75f0d138c8ed44
wahidahmed <wahidahmed1992@gmail.com>
2023-03-19 19:14:16 +0800

    generate initail server with port

---
+++ AttendanceProject/server.js
@@ -0,0 +1,12 @@
+const express=require('express');
+
+const app=express();
+
+app.listen(4000,()=>{
+    console.log('i am listening from port:4000');
+})
+
+
+app.get('/',(req,res,next)=>{
+    res.send('thank for your request');
+});
