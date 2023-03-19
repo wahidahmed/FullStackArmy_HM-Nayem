0b219a656283189a6b7c4c07f94021fef4554d24
wahidahmed <wahidahmed1992@gmail.com>
2023-03-19 20:32:10 +0800

    create user Model and schema

---
+++ AttendanceProject/models/user.js
@@ -0,0 +1,14 @@
+// const mongoose=require('mongoose');
+const {model,Schema}=require('mongoose');
+
+const userSchema=new Schema({
+    name:String,
+    email:String,
+    password:String,
+    roles:[String],
+    accountStatus:String
+})
+
+const User=model('User',userSchema);
+
+module.exports=User;
