e8e56f90b097efa3fe7ecfadb9efbc8c7565bba4
wahidahmed <wahidahmed1992@gmail.com>
2023-04-10 21:26:16 +0800

    add user with database

---
+++ AttendanceProject/db.js
@@ -0,0 +1,8 @@
+
+const mongoose=require('mongoose');
+
+function connectDB(connectionString){
+    return mongoose.connect(connectionString)
+}
+
+module.exports=connectDB;
--- AttendanceProject/server.js
+++ AttendanceProject/server.js
@@ -1,30 +1,39 @@
 const express=require('express');
+const connectDB=require('./db');
+const User=require('./models/User');
+const bcrypt=require('bcryptjs');
 
 const app=express();
 
-app.listen(4000,()=>{
-    console.log('i am listening from port:4000');
-})
+app.use(express.json())
 
-
-app.get('/',(req,res,next)=>{
-    res.send('thanks for your request');
-});
-
-const {model,Schema}=require('mongoose');
-
-const studentAttendenceSchema=new Schema({
-    createdAt: Date,
-    user:{
-        type:Schema.Types.ObjectId,
-        ref:'User'
-    },
-    adminAttendence:{
-        type:Schema.Types.ObjectId,
-        ref:'AdminAttendence'
+app.post('/register',async (req,res)=>{
+    const{name,email,password}=req.body;
+    if(!name|| !email || !password){
+        return res.status(400).json({message:'Invalid data'});
     }
+
+  let user= await  User.findOne({email:email});
+  if(user){
+   return res.status(400).json({message:'user already exist'});
+  }
+
+  user=new User({
+    name:name,
+    email:email,
+    password:password
+  })
+  await user.save();
+  return res.status(201).json({message:'Successully User Created',user });
 })
 
-const StudentAttendence=model('StudentAttendence',studentAttendenceSchema);
+connectDB('mongodb://localhost:27017/attendance-db')
+.then(()=>{
+    console.log('database connected');
+    app.listen(4000,()=>{
+        console.log('i am listening from port:4000');
+    })
 
-module.exports=StudentAttendence;
+}).catch((e)=>{
+    console.log(e);
+})
