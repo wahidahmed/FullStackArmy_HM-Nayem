d064a64d7f9bfe386d6da5a67d62655ad3f400a1
wahidahmed <wahidahmed1992@gmail.com>
2023-04-22 10:30:41 +0800

    refector attendence projec

--- AttendanceProject/server.js
+++ AttendanceProject/server.js
@@ -1,69 +1,20 @@
 const express=require('express');
 const connectDB=require('./db');
-const User=require('./models/User');
-const bcrypt=require('bcryptjs');
-const jwt=require('jsonwebtoken');
 const authentcate=require('./middleware/authenticate');
+const routes=require('./routes/index');
 
 const app=express();
-
 app.use(express.json())
+app.use(routes);
+
 app.use((err,req,res,next)=>{
-    console.log(err);
-    res.status(500).json({message:'Server error occured'})
+    console.log('err',err);
+    const message=err.message?err.message:'Serve error occured';
+    const status=err.status?err.status:500;
+    res.status(status).json({message:message})
 })
 
-app.post('/register',async (req,res,next)=>{
-    const{name,email,password}=req.body;
-    if(!name|| !email || !password){
-        return res.status(400).json({message:'Invalid data'});
-    }
-    try {
-        
-            let user= await  User.findOne({email:email});
-            if(user){
-            return res.status(400).json({message:'user already exist'});
-            }
 
-            user=new User({
-                name:name,
-                email:email,
-                password:password
-            })
-
-            const salt= await bcrypt.genSalt(10);
-            const hash= await bcrypt.hash(password,salt);
-
-            user.password=hash;
-
-            await user.save();
-            return res.status(201).json({message:'Successully User Created',user });
-    } 
-    catch (error) {
-        next(error);
-    }
-})
-
-app.post('/login',async(req,res,next)=>{
-    const{email,password}=req.body;
-    try {
-      const user=  await User.findOne({email});
-      if(!user){
-        return res.status(400).json({message:'Invalid Credential'});
-      }
-    const isMatch=await bcrypt.compare(password,user.password);
-    if(!isMatch){
-        return res.status(400).json({message:'Invalid Credential'});
-    }
-
-    delete user._doc.password;
-
-    const token =jwt.sign(user._doc,'secret-key',{expiresIn:'2h'});
-    return res.status(200).json({message:'Login successfull',token});
-    } catch (e) {
-        next(e);
-    }
-})
 
 app.get('/private',authentcate,(req,res)=>{
    res.status(200).json({message:'I am private route'});
---
+++ AttendanceProject/service/auth.js
@@ -0,0 +1,53 @@
+
+
+const jwt=require('jsonwebtoken');
+const bcrypt=require('bcryptjs');
+const error=require('../Utils/error');
+const{findUserByProperty,createNewUser}=require('./user');
+
+const registerService=async ({name,email,password})=>{
+    let user= await  findUserByProperty('email',email);
+    if(user){
+      throw error('User already exist',400);
+    
+    }
+
+    const salt= await bcrypt.genSalt(10);
+    const hash= await bcrypt.hash(password,salt);
+
+    return createNewUser({name,email,password:hash});
+
+   
+}
+
+const loginService= async({email,password})=>{
+ 
+    const user= await findUserByProperty('email',email);
+    console.log('illegal',user.password)
+    if(!user){
+      throw error('Invalid Credential',400);
+    }
+  const isMatch=await bcrypt.compare(password,user.password);
+ 
+  if(!isMatch){
+    throw error('Invalid Credential',400);
+  }
+
+  // delete user._doc.password;
+
+  const payload={
+    _id:user._id,
+    name:user.name,
+    email:user.email,
+    roles:user.roles,
+    accountStatus:user.accountStatus
+  }
+
+  // const token =jwt.sign(user._doc,'secret-key',{expiresIn:'2h'});
+  return jwt.sign(payload,'secret-key',{expiresIn:'2h'});
+}
+
+module.exports={
+    registerService,
+    loginService
+}
